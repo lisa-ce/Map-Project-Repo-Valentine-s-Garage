fada7eff35ae6f7dd63ab93c3f496223517025c7
Daholva N.V Ndjahera <nnaledi1166@gmail.com>
2026-05-21 12:11:43 +0200

    Update MainActivity and add Firebase Auth dependency

--- app/src/main/java/com/example/mapprojectvalentinesgaragemanagementsystem/MainActivity.kt
+++ app/src/main/java/com/example/mapprojectvalentinesgaragemanagementsystem/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.mapprojectvalentinesgaragemanagementsystem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.example.mapprojectvalentinesgaragemanagementsystem.navigation.AppNavigation
+import com.example.mapprojectvalentinesgaragemanagementsystem.ui.theme.MapProjectValentinesGarageManagementSystemTheme
 
+/**
+ * Entry point for Valentine's Garage Management app.
+ */
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
-
-            AppNavigation()
-
+            MapProjectValentinesGarageManagementSystemTheme {
+                AppNavigation()
+            }
         }
-
     }
-
 }
