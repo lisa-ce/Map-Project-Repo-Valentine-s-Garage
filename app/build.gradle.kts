0d02056633bcd3d11e15b7f2f35c2e8c1d03893b
Daholva N.V Ndjahera <nnaledi1166@gmail.com>
2026-05-21 12:11:02 +0200

    Add firebase-auth dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
